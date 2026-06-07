e61b9599690846d48b8b69b26d2f5932250840d4
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-06-07 20:22:52 +0100

    Adding generic error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,17 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <h1>Oops...</h1>
+      <p>
+        {isRouteErrorResponse(error)
+          ? "This page could not be found."
+          : "An unexpected error occurred."}
+      </p>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import Layout from "./pages/Layout";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import PrivateRoutes from "./pages/PrivateRoutes";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/login",
