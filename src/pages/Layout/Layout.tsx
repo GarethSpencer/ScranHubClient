07a9cfd2f5b7e533b46e5c6927bf9a4783ae3bf9
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-06-09 21:11:36 +0100

    Setting up the logout button on the private layout screen.

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -3,20 +3,33 @@ import "./Layout.css";
 import { Outlet } from "react-router-dom";
 import DarkModeContext from "../../contexts/darkModeContext";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
+import { Button } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Layout = () => {
   const { state: isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
   const copyrightYear = new Date().getFullYear();
+  const { logout } = useAuth0();
 
   return (
     <div className="main-layout-container">
       <div className="content-wrapper">
-        <button
-          className="position-absolute top-0 end-0 m-3 border-0 bg-transparent fs-4 theme-toggle-btn"
-          onClick={() => toggleDarkMode({ type: "SET" })}
-        >
-          {isDarkMode ? <MdLightMode /> : <MdDarkMode />}
-        </button>
+        <div>
+          <Button
+            className="btn-primary position-absolute top-0 start-0 m-3 border-0 bg-transparent logout-btn"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Logout
+          </Button>
+          <button
+            className="position-absolute top-0 end-0 m-3 border-0 bg-transparent fs-4 theme-toggle-btn"
+            onClick={() => toggleDarkMode({ type: "SET" })}
+          >
+            {isDarkMode ? <MdLightMode /> : <MdDarkMode />}
+          </button>
+        </div>
         <Outlet />
       </div>
       <div className="content-footer">
