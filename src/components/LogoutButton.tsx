78fc6e88e446ffeacdea91db96022bbf50d7392a
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-06-10 12:54:07 +0100

    Adding custom scss stylesheet for colour scheme

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -6,7 +6,7 @@ const LogoutButton = () => {
 
   return (
     <Button
-      className="btn-primary position-absolute top-0 start-0 m-3 border-0 bg-transparent logout-btn"
+      className="position-absolute top-0 start-0 m-3 border-0 bg-transparent logout-btn"
       onClick={() =>
         logout({ logoutParams: { returnTo: window.location.origin } })
       }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AuthProvider from "./auth/AuthProvider";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/custom.scss";
 import router from "./routes";
 import { RouterProvider } from "react-router-dom";
 import DarkModeProvider from "./contexts/darkMode/DarkModeProvider";
