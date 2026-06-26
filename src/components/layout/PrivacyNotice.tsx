852a54fd67de9cd8e6654b24ed03dda5e2250816
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-06-26 14:43:17 +0100

    Adding a privacy notice banner to the login page to notify the user of the use of local storage

---
+++ src/components/layout/PrivacyNotice.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const STORAGE_KEY = "privacyNoticeDismissed";
+
+const PrivacyNotice = () => {
+  const [dismissed, setDismissed] = useState(
+    () => localStorage.getItem(STORAGE_KEY) === "true",
+  );
+
+  if (dismissed) return null;
+
+  const dismiss = () => {
+    localStorage.setItem(STORAGE_KEY, "true");
+    setDismissed(true);
+  };
+
+  return (
+    <button
+      type="button"
+      className="privacy-notice"
+      onClick={dismiss}
+      aria-label="Dismiss privacy notice"
+    >
+      Your theme preference and login session will be stored locally in your
+      browser. This site doesn't use any tracking or advertising cookies.
+      <span className="privacy-notice-action">Got it</span>
+    </button>
+  );
+};
+
+export default PrivacyNotice;
--- src/layout/LoginLayout.tsx
+++ src/layout/LoginLayout.tsx
@@ -2,10 +2,12 @@ import "./Layout.scss";
 import LoginPage from "../pages/LoginPage";
 import DarkModeButton from "../components/layout/DarkModeButton";
 import LoginLayoutFooter from "../components/layout/LoginLayoutFooter";
+import PrivacyNotice from "../components/layout/PrivacyNotice";
 
 const LoginLayout = () => {
   return (
     <div className="main-layout-container">
+      <PrivacyNotice />
       <div className="login-content-wrapper mx-2">
         <DarkModeButton />
         <LoginPage />
