0b5e7b1a584dadd263f66575138419d37714d00f
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-07-04 12:35:46 +0100

    Setting placeholders in group venue tables to be centered instead of left aligned to match response data layout

--- src/components/GroupVenueSkeletonRow.tsx
+++ src/components/GroupVenueSkeletonRow.tsx
@@ -2,7 +2,7 @@ import Placeholder from "react-bootstrap/Placeholder";
 
 const GroupVenueSkeletonRow = () => (
   <tr aria-hidden="true">
-    <Placeholder as="td" animation="glow" className="text-start">
+    <Placeholder as="td" animation="glow">
       <Placeholder xs={8} />
     </Placeholder>
     <Placeholder as="td" animation="glow">
