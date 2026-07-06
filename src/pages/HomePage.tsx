4d589641e65dfb8d443ca11d76f8366d5e3342de
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-07-06 12:17:13 +0100

    Updating title to > 15 characters for better indexing. Setting h1 element on the home page statically before the app gets hydrated.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,9 @@ import MenuContainer from "../components/MenuContainer";
 const HomePage = () => {
   return (
     <div className="content-wrapper">
-      <h1 className="visually-hidden">Home</h1>
+      <h1 className="visually-hidden">
+        ScranHub — plan and rate your scranning adventures with friends
+      </h1>
       <MenuContainer />
     </div>
   );
