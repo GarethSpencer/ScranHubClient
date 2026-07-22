9fb82fda5f7a6e46f38a8b4e7fd99df3eea917f4
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-07-22 13:16:59 +0100

    Updating visited on date field to not show to the user until the venue has been visited, to make the modal cleaner instead of just disabling the input.

--- src/components/venue/VenueDetailsFields.tsx
+++ src/components/venue/VenueDetailsFields.tsx
@@ -91,18 +91,20 @@ const VenueDetailsFields = ({ form, isPending }: Props) => {
             />
           </Form.Group>
         </Col>
-        <Col xs={6}>
-          <Form.Group controlId="updateVenueVisitedOn">
-            <Form.Label>Visited On</Form.Label>
-            <Form.Control
-              type="date"
-              value={values.visitedOn}
-              onChange={(e) => setters.setVisitedOn(e.target.value)}
-              disabled={isPending || !values.visited}
-              max={form.todayDateInputValue}
-            />
-          </Form.Group>
-        </Col>
+        {values.visited && (
+          <Col xs={6}>
+            <Form.Group controlId="updateVenueVisitedOn">
+              <Form.Label>Visited On</Form.Label>
+              <Form.Control
+                type="date"
+                value={values.visitedOn}
+                onChange={(e) => setters.setVisitedOn(e.target.value)}
+                disabled={isPending}
+                max={form.todayDateInputValue}
+              />
+            </Form.Group>
+          </Col>
+        )}
       </Row>
     </>
   );
